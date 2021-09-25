99e0eb4f535c386744ef5ea6ebc9a17ec8db8e8d
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-25 18:22:42 -0300

    add  Tarefa.cs

---
+++ FuncionariosApi/Business/Entities/Tarefa.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace FuncionariosApi.Business.Entities
+{
+    public class Tarefa
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TarefaID { get; set; }
+        [StringLength(50)]
+        public string Nome { get; set; }
+        
+        public DateType DataEntrega { get; set; }
+        [StringLength(150)]
+        public string DescricaoTarefa { get; set; }
+        [StringLength(15)]
+        public string Status { get; set; }
+    }
+}
