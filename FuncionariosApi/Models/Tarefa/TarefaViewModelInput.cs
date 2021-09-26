f8208ea1d6a420e190af38cbe776a77e9fffdbc4
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-26 10:47:31 -0300

    add TarefaViewModelInput.cs

---
+++ FuncionariosApi/Models/Tarefa/TarefaViewModelInput.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace FuncionariosApi.Models.Tarefa
+{
+    public class TarefaViewModelInput
+    {
+        [Required(ErrorMessage = "o Campo Nome é obrigatório. ")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "o Campo Data da Entrega é obrigatório. ")]
+        public DateType DataEntrega { get; set; }
+        [Required(ErrorMessage = "o Campo Descrição da Tarefa é obrigatório. ")]
+        public string DescricaoTarefa { get; set; }
+        [Required(ErrorMessage = "o Campo Status é obrigatório. ")]
+        public string Status { get; set; }
+    }
+}
