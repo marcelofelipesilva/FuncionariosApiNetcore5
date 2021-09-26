d9c55d0cb7920e911cba97d7fa97cb2cc13d35c9
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-26 11:15:17 -0300

    add migrations.

---
+++ FuncionariosApi/Migrations/20210926135814_incialProjeto.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FuncionariosApi.Migrations
+{
+    public partial class incialProjeto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB_Funcionario",
+                columns: table => new
+                {
+                    FuncionarioID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Matricula = table.Column<int>(type: "int", maxLength: 25, nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: true),
+                    DataAdmissao = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    Cargo = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    DetalhesFuncao = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_Funcionario", x => x.FuncionarioID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TB_Tarefa",
+                columns: table => new
+                {
+                    TarefaID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    DataEntrega = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DescricaoTarefa = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_Tarefa", x => x.TarefaID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB_Funcionario");
+
+            migrationBuilder.DropTable(
+                name: "TB_Tarefa");
+        }
+    }
+}
