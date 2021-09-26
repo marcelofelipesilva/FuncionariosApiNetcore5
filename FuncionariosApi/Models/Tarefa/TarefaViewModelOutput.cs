fcc76cbd80b0ef0cb2bd01e2b8e5e0333df37004
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-26 11:16:03 -0300

    add TarefaViewModelOutput.cs

--- FuncionariosApi/Models/Tarefa/TarefaViewModelOutput.cs
+++ FuncionariosApi/Models/Tarefa/TarefaViewModelOutput.cs
@@ -5,7 +5,7 @@ namespace FuncionariosApi.Models.Tarefa
     public class TarefaViewModelOutput
     {
         public string Nome { get; set; }
-        public DateType DataEntrega { get; set; }
+        public string DataEntrega { get; set; }
         public string DescricaoTarefa { get; set; }
         public string Status { get; set; }
     }
